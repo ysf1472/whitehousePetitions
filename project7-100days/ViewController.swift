3b92e8466481f2b31b00f1edbce0934c3f9ae071
ysf1472 <yusuf.ahmet.nihat@gmail.com>
2022-10-19 13:30:15 -0400

    Created the blueprint of the project

--- project7-100days/ViewController.swift
+++ project7-100days/ViewController.swift
@@ -8,12 +8,26 @@
 import UIKit
 
 class ViewController: UITableViewController {
-
+var petitions = [String]()
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+        
+    }
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell",for: indexPath)
+        var content = cell.defaultContentConfiguration()
+        content.text="Title goes Here"
+        content.secondaryText="Subtitle goes here"
+        cell.contentConfiguration = content
+        return cell
+    }
 
 }
 
